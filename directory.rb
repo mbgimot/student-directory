5811ee979b698d8f1680df8e98096d1110ff1fe0
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2016-12-15 21:13:04 +0000

    8.ex1

--- directory.rb
+++ directory.rb
@@ -23,9 +23,10 @@ def print_header
 end
 
 def print(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, index|
+  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
 end
+
 end
 #finally, we print the total number of students
 def print_footer(students)
