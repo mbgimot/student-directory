e3689873027cb9c01becc54513163b04731c96aa
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2016-12-15 22:01:04 +0000

    15decfinish

---
+++ 8_ex1.rb
@@ -0,0 +1,39 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an emprty array
+  students = []
+  #get first name
+  name = $stdin.gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = $stdin.gets.chomp
+  end
+
+  #return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+students.each_with_index do |student, index|
+  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+end
+end
+#finally, we print the total number of students
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+#nothing happens until we call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
---
+++ 8_ex3.rb
@@ -0,0 +1,43 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an emprty array
+  students = []
+  #get first name
+  name = $stdin.gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = $stdin.gets.chomp
+  end
+
+  #return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+students.each_with_index do |student, index|
+  string_name = student[:name].to_s.downcase
+  if string_name.length < 12
+  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+end
+end
+
+end
+#finally, we print the total number of students
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+#nothing happens until we call the methods
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
