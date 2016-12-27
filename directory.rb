8b731bafd14e293bb7da1aca5fdc768fa9908934
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2016-12-27 15:29:32 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -34,8 +34,26 @@ def print_footer(students)
 end
 #nothing happens until we call the methods
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
